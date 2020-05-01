d196c6ce76a63c34b3e81d3293c3ca055f12d74d
Anunay Kumar <kumaranunay123@gmail.com>
2020-05-01 22:20:38 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#Run as root user
+export JFROG_HOME=/jfrog
+mkdir -p $JFROG_HOME/artifactory/var/etc/
+cd $JFROG_HOME/artifactory/var/etc/
+touch ./system.yaml
+chown -R 1030:1030 $JFROG_HOME/artifactory/var
+
+#Docker image
+docker run --name artifactory -v $JFROG_HOME/artifactory/var/:/var/opt/jfrog/artifactory -d -p 8081:8081 -p 8082:8082 docker.bintray.io/jfrog/artifactory-pro
